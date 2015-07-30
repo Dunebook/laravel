22e215cda9169bf1b497b6378d3ff2f10449e093
hyguyz <hyguyz@outlook.com>
2015-07-30 10:43:17 +0530

    added required routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,15 +2,25 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| application Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|---------------------------------------------------------------------------------
+| here i will register all routes for register login e.tc @deven rathore
+|----------------------------------------------------------------------------------
+ */
+Route::get('/', ['middleware' => 'guest', 'uses' => 'homecontroller@hyguyz']);
+
+
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
