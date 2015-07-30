94f0d575880d009029430b953fb3b76e8ecd2ab0
hyguyz <hyguyz@outlook.com>
2015-07-30 10:29:25 +0530

    added username field for login form

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,64 +2,127 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
-use Validator;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Http\Request; 
+use Validator;
+use Laravel\Socialite\Facades\Socialite;
 
-class AuthController extends Controller
+class AuthController extends Controller {
+	/*
+	|--------------------------------------------------------------------------
+	| Registration & Login Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the registration of new users, as well as the
+	| authentication of existing users. By default, this controller uses
+	| a simple trait to add these behaviors. Why don't you explore it?
+	|
+	 */
+
+	use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+
+
+
+
+
+
+
+
+public function postLogin(Request $request)
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
+    // get our login input
+    $login = $request->input('login');
 
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+    // check login field
+    $login_type = filter_var( $login, FILTER_VALIDATE_EMAIL ) ? 'email' : 'username';
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest', ['except' => 'getLogout']);
+    // merge our login field into the request with either email or username as key
+    $request->merge([ $login_type => $login ]);
+
+    // let's validate and set our credentials
+    if ( $login_type == 'email' ) {
+
+        $this->validate($request, [
+            'email'    => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only( 'email', 'password' );
+
+    } else {
+
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only( 'username', 'password' );
+
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
+if (\Auth::attempt($credentials,'remember'))
     {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
+        return redirect()->intended($this->redirectPath());
+    }
+
+    return redirect($this->loginPath())
+        ->withInput($request->only('login', 'remember'))
+        ->withErrors([
+            'login' => $this->getFailedLoginMessage(),
+        ]);
+}
+
+
+
+
+
+ 
+	protected $redirectPath = '/dashboard';
+	protected $loginPath = '/';
+
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		$this->middleware('guest', ['except' => 'getLogout']);
+	}
+
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	protected function validator(array $data) {
+		return Validator::make($data, [
+			'name' => 'required|max:255',
+			'email' => 'required|email|max:255|unique:users',
+		    'username' => 'required|max:255|unique:users',
+
             'password' => 'required|confirmed|min:6',
-        ]);
-    }
+		]);
+	}
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return User
+	 */
+	protected function create(array $data) {
+		return User::create([
+			'name' => $data['name'],
+			'email' => $data['email'],
+			'username' => $data['username'],
+
+			'password' => bcrypt($data['password']),
+		]);
+
+	}
 }
